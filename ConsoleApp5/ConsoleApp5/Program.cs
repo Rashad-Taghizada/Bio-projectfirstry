acb42989b6614c7bd0924573c7817202f705165f
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-16 19:42:40 +0100

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+
+          public void Drive(double km)
+        {
+            double lazimOlanYanacaq = km * FuelFor1Km;
+
+            if (CurrentFuel >= lazimOlanYanacaq)
+
+            { 
+                CurrentFuel = CurrentFuel - lazimOlanYanacaq;
+                Console.WriteLine($"{km} km gedildi. Qalan benzin: {CurrentFuel} litr.");
+            }
+            else
+            {
+                // Benzin yetmiyorsa uyarı ver
+                Console.WriteLine("Bu məsafəni getmək üçün kifayət qədər benzin yoxdur!");
+            }
+        }
+    }
+
+}
+
---
+++ ConsoleApp5/ConsoleApp5/vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class vehicle
+    {
+        public string Color;
+        public int Year; // Buraxılış ili
+        public string Brand;
+        public string Model;
+        public double FuelCapacity;
+        public double FuelFor1Km;
+        public double CurrentFuel;
+        public Vehicle(int year)
+        {
+            this.Year = year;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"--- Maşın Məlumatı ---");
+            Console.WriteLine($"Marka/Model: {Brand} {Model}");
+            Console.WriteLine($"İl: {Year} | Rəng: {Color}");
+            Console.WriteLine($"Yanacaq: {CurrentFuel}L / {FuelCapacity}L");
+
+        }
+    }
